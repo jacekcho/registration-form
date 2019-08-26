89f0efeeee51ef3648ddbf39e8a00f48a13c3582
jacek.chowaniec <jacek.chowaniec85@gmail.com>
2019-08-26 18:09:01 +0200

    init commit

---
+++ src/Menu/Menu.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+import {userActions} from '../_actions';
+
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+
+        // reset login status
+        this.props.logout();
+    }
+
+    render() {
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h2>Menu</h2>
+                <div className="form-group">
+                    <Link to="/login" className="btn btn-link">Login</Link>
+                    <Link to="/register" className="btn btn-link">Register</Link>
+                </div>
+            </div>
+        );
+    }
+}
+
+function mapState(state) {
+    const {loggingIn} = state.authentication;
+    return {loggingIn};
+}
+
+const actionCreators = {
+    logout: userActions.logout
+};
+
+const connectedMenu = connect(mapState, actionCreators)(Menu);
+export {connectedMenu as Menu};
